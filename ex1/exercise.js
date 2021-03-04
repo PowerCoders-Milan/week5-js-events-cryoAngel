2b681f49469fdac56c3aaab480cdffb8812c7f1d
Grace Kabeya <antoinefaith1@gmail.com>
2021-03-04 12:12:37 +0100

    finished

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,5 +1,11 @@
 // define your functions here
 var a = document.querySelector('a');
+var strong = document.querySelectorAll('strong');
+a.addEventListener('mouseover', function (){
+    strong.forEach(function(strong){
+        strong.style.color = "blue";
+    })
+})
 a.addEventListener('mouseover',function boldWords(){
     a.style.fontWeight = "bold";
 })
