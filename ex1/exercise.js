8447500b344d31cfb74c8c773f4e265e8bda6ef9
Grace Kabeya <antoinefaith1@gmail.com>
2021-03-04 10:56:47 +0100

    finished

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,9 @@
 // define your functions here
-
+var a = document.querySelector('a');
+a.addEventListener('mouseover',function boldWords(){
+    a.style.fontWeight = "bold";
+})
 // call the functions and connect them to an event
+a.addEventListener('mouseout', function remove(){
+    a.style.fontWeight= "normal";
+})
